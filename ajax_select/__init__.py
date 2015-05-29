17f56d4a264fc06acad60c6163c96976828886c5
Tony Morrow <tony.morrow@gmail.com>
2015-05-28 23:06:33 -0500

    Initial work stubbing out autodiscovery

--- ajax_select/__init__.py
+++ ajax_select/__init__.py
@@ -12,6 +12,8 @@ from django.forms.models import ModelForm
 from django.utils.text import capfirst
 from django.utils.html import escape
 from django.utils.translation import ugettext_lazy as _
+from django.utils.module_loading import autodiscover_modules
+from django.contrib.admin.sites import site
 
 
 class LookupChannel(object):
@@ -163,12 +165,14 @@ def make_ajax_field(model, model_fieldname, channel, show_help_text=False, **kwa
 
 def get_lookup(channel):
     """ find the lookup class for the named channel.  this is used internally """
+
     try:
-        lookup_label = settings.AJAX_LOOKUP_CHANNELS[channel]
+        lookup_label = channels[channel]
     except AttributeError:
         raise ImproperlyConfigured("settings.AJAX_LOOKUP_CHANNELS is not configured")
     except KeyError:
-        raise ImproperlyConfigured("settings.AJAX_LOOKUP_CHANNELS not configured correctly for %r" % channel)
+        raise ImproperlyConfigured("settings.AJAX_LOOKUP_CHANNELS not configured correctly for %(channel)r "
+                                   "or autodiscovery could not locate %(channel)r" % channel)
 
     if isinstance(lookup_label, dict):
         # 'channel' : dict(model='app.model', search_field='title' )
@@ -212,3 +216,39 @@ def make_channel(app_model, arg_search_field):
         search_field = arg_search_field
 
     return MadeLookupChannel()
+
+
+class AutoDiscover():
+
+    def __getitem__(self, key):
+        if not hasattr(self, 'channels'):
+            self.channels = self.discover_channels()
+        return self.channels[key]
+
+    @staticmethod
+    def merge_dict(d1, d2):
+        """
+        Merges two dictionaries into one new dict using copy()
+        :param d1: Dict()
+        :param d2: Dict()
+        :return: Dict()
+        """
+        merged = d1.copy()
+        merged.update(d2)
+        return merged
+
+    @staticmethod
+    def discover_channels():
+        channels = {}
+        autodiscover_modules('lookup', register_to=site)
+        for module in site._registry:
+            try:
+                if hasattr(module, "AJAX_LOOKUP_CHANNELS"):
+                    channels = AutoDiscover.merge_dict(channels, module.AJAX_LOOKUP_CHANNELS)
+            except:
+                pass
+
+        return channels
+
+
+channels = settings.AJAX_LOOKUP_CHANNELS if hasattr(settings, 'AJAX_LOOKUP_CHANNELS') else AutoDiscover()
