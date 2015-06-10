e398768a2a0aadb5e49a16d2bf898613c3aa12a8
morr0350 <tony.morrow@gmail.com>
2015-06-09 21:26:42 -0500

    just adding basic admin.py

--- ajax_select_tests/admin.py
+++ ajax_select_tests/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
+from ajax_select import make_ajax_form
 from ajax_select.admin import AjaxSelectAdmin
 
 from .models import TestUser
 
 @admin.register(TestUser)
 class TestUserAdmin(AjaxSelectAdmin):
-    pass
+    form = make_ajax_form(TestUser)
